8a30c217981eb91c670fb85c791705d0e091084c
GercekSOLMAZ <gerceksolmaz463@gmail.com>
2024-04-08 20:49:54 +0300

    üye işlemleri sql bağlantısı oluşturuldu. tablo oluşturuldu. eklme silme ve güncellme komutları yazıldı.

--- gorselprogramlamavize/Form1.Designer.cs
+++ gorselprogramlamavize/Form1.Designer.cs
@@ -39,13 +39,16 @@
             label4 = new Label();
             uyesilmebutonu = new Button();
             uyeguncellemebutonu = new Button();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // uyeeklemebutonu
             // 
-            uyeeklemebutonu.Location = new Point(12, 230);
+            uyeeklemebutonu.Location = new Point(14, 307);
+            uyeeklemebutonu.Margin = new Padding(3, 4, 3, 4);
             uyeeklemebutonu.Name = "uyeeklemebutonu";
-            uyeeklemebutonu.Size = new Size(75, 23);
+            uyeeklemebutonu.Size = new Size(86, 31);
             uyeeklemebutonu.TabIndex = 0;
             uyeeklemebutonu.Text = "ekle";
             uyeeklemebutonu.UseVisualStyleBackColor = true;
@@ -54,90 +57,108 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(5, 40);
+            label1.Location = new Point(6, 53);
             label1.Name = "label1";
-            label1.Size = new Size(29, 15);
+            label1.Size = new Size(36, 20);
             label1.TabIndex = 1;
             label1.Text = "İsim";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(65, 37);
+            textBox1.Location = new Point(74, 49);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
+            textBox1.Size = new Size(114, 27);
             textBox1.TabIndex = 2;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(5, 94);
+            label2.Location = new Point(6, 125);
             label2.Name = "label2";
-            label2.Size = new Size(51, 15);
+            label2.Size = new Size(64, 20);
             label2.TabIndex = 1;
             label2.Text = "Soy İsim";
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(65, 86);
+            textBox2.Location = new Point(74, 115);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(100, 23);
+            textBox2.Size = new Size(114, 27);
             textBox2.TabIndex = 2;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(5, 134);
+            label3.Location = new Point(6, 179);
             label3.Name = "label3";
-            label3.Size = new Size(20, 15);
+            label3.Size = new Size(25, 20);
             label3.TabIndex = 1;
             label3.Text = "TC";
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(65, 126);
+            textBox3.Location = new Point(74, 168);
+            textBox3.Margin = new Padding(3, 4, 3, 4);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(100, 23);
+            textBox3.Size = new Size(114, 27);
             textBox3.TabIndex = 2;
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(65, 172);
+            textBox4.Location = new Point(74, 229);
+            textBox4.Margin = new Padding(3, 4, 3, 4);
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(100, 23);
+            textBox4.Size = new Size(114, 27);
             textBox4.TabIndex = 4;
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(5, 180);
+            label4.Location = new Point(6, 240);
             label4.Name = "label4";
-            label4.Size = new Size(45, 15);
+            label4.Size = new Size(58, 20);
             label4.TabIndex = 3;
             label4.Text = "E posta";
             // 
             // uyesilmebutonu
             // 
-            uyesilmebutonu.Location = new Point(93, 230);
+            uyesilmebutonu.Location = new Point(106, 307);
+            uyesilmebutonu.Margin = new Padding(3, 4, 3, 4);
             uyesilmebutonu.Name = "uyesilmebutonu";
-            uyesilmebutonu.Size = new Size(75, 23);
+            uyesilmebutonu.Size = new Size(86, 31);
             uyesilmebutonu.TabIndex = 5;
             uyesilmebutonu.Text = "sil";
             uyesilmebutonu.UseVisualStyleBackColor = true;
+            uyesilmebutonu.Click += uyesilmebutonu_Click;
             // 
             // uyeguncellemebutonu
             // 
-            uyeguncellemebutonu.Location = new Point(174, 230);
+            uyeguncellemebutonu.Location = new Point(199, 307);
+            uyeguncellemebutonu.Margin = new Padding(3, 4, 3, 4);
             uyeguncellemebutonu.Name = "uyeguncellemebutonu";
-            uyeguncellemebutonu.Size = new Size(75, 23);
+            uyeguncellemebutonu.Size = new Size(86, 31);
             uyeguncellemebutonu.TabIndex = 6;
             uyeguncellemebutonu.Text = "güncelle";
             uyeguncellemebutonu.UseVisualStyleBackColor = true;
+            uyeguncellemebutonu.Click += uyeguncellemebutonu_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(291, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(611, 494);
+            dataGridView1.TabIndex = 7;
             // 
             // uye_ekleme
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 600);
+            Controls.Add(dataGridView1);
             Controls.Add(uyeguncellemebutonu);
             Controls.Add(uyesilmebutonu);
             Controls.Add(textBox4);
@@ -149,8 +170,11 @@
             Controls.Add(textBox1);
             Controls.Add(label1);
             Controls.Add(uyeeklemebutonu);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "uye_ekleme";
             Text = "üye ekleme";
+            Load += uye_ekleme_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -168,5 +192,6 @@
         private Label label4;
         private Button uyesilmebutonu;
         private Button uyeguncellemebutonu;
+        private DataGridView dataGridView1;
     }
 }
--- gorselprogramlamavize/Form1.cs
+++ gorselprogramlamavize/Form1.cs
@@ -1,20 +1,124 @@
+using System;
+using System.Data;
+using System.Data.SQLite;
+using System.Windows.Forms;
+
 namespace gorselprogramlamavize
 {
     public partial class uye_ekleme : Form
     {
+        private SQLiteConnection connection;
+        private SQLiteDataAdapter dataAdapter;
+        private DataTable dataTable;
+
         public uye_ekleme()
         {
             InitializeComponent();
+            InitializeDatabase();
         }
 
-        private void label3_Click(object sender, EventArgs e)
+        private void InitializeDatabase()
         {
+            connection = new SQLiteConnection("Data Source=database.db;Version=3;");
+            string createTableQuery = @"CREATE TABLE IF NOT EXISTS Uyeler (
+                                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                        isim TEXT,
+                                        soyisim TEXT,
+                                        tc TEXT,
+                                        eposta TEXT)";
 
+            SQLiteCommand command = new SQLiteCommand(createTableQuery, connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        private void LoadData()
+        {
+            dataAdapter = new SQLiteDataAdapter("SELECT * FROM Uyeler", connection);
+            dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;
+        }
+
+        private void uye_ekleme_Load(object sender, EventArgs e)
+        {
+            LoadData();
         }
 
         private void uyeeklemebutonu_Click(object sender, EventArgs e)
         {
+            connection.Open();
+            string insertQuery = @"INSERT INTO Uyeler (isim, soyisim, tc, eposta) 
+                                    VALUES (@isim, @soyisim, @tc, @eposta)";
 
+            SQLiteCommand command = new SQLiteCommand(insertQuery, connection);
+            command.Parameters.AddWithValue("@isim", textBox1.Text);
+            command.Parameters.AddWithValue("@soyisim", textBox2.Text);
+            command.Parameters.AddWithValue("@tc", textBox3.Text);
+            command.Parameters.AddWithValue("@eposta", textBox4.Text);
+
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            MessageBox.Show("Üye baþarýyla eklendi.");
+
+            // Verileri yeniden yükle
+            LoadData();
+        }
+
+        private void uyesilmebutonu_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                connection.Open();
+                string deleteQuery = "DELETE FROM Uyeler WHERE id=@id";
+
+                SQLiteCommand command = new SQLiteCommand(deleteQuery, connection);
+                command.Parameters.AddWithValue("@id", dataGridView1.SelectedRows[0].Cells["id"].Value);
+
+                command.ExecuteNonQuery();
+                connection.Close();
+
+                MessageBox.Show("Üye baþarýyla silindi.");
+
+                // Verileri yeniden yükle
+                LoadData();
+            }
+            else
+            {
+                MessageBox.Show("Lütfen bir satýr seçin.");
+            }
+        }
+
+        private void uyeguncellemebutonu_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                connection.Open();
+                string updateQuery = @"UPDATE Uyeler 
+                                        SET isim=@isim, soyisim=@soyisim, tc=@tc, eposta=@eposta 
+                                        WHERE id=@id";
+
+                SQLiteCommand command = new SQLiteCommand(updateQuery, connection);
+                command.Parameters.AddWithValue("@isim", textBox1.Text);
+                command.Parameters.AddWithValue("@soyisim", textBox2.Text);
+                command.Parameters.AddWithValue("@tc", textBox3.Text);
+                command.Parameters.AddWithValue("@eposta", textBox4.Text);
+                command.Parameters.AddWithValue("@id", dataGridView1.SelectedRows[0].Cells["id"].Value);
+
+                command.ExecuteNonQuery();
+                connection.Close();
+
+                MessageBox.Show("Üye baþarýyla güncellendi.");
+
+                // Verileri yeniden yükle
+                LoadData();
+            }
+            else
+            {
+                MessageBox.Show("Lütfen bir satýr seçin.");
+            }
         }
     }
 }
