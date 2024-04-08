ad74a716e3c1e5c02b0f59a8e3968ba87bdc9d5e
GercekSOLMAZ <gerceksolmaz463@gmail.com>
2024-04-08 20:45:10 +0300

    kitap ekleme sql bağlantısı kuruldu. tablo oluşturuldu. veri ekleme silme ve güncellme kodalrı eklendi

--- gorselprogramlamavize/kitap_ekle.Designer.cs
+++ gorselprogramlamavize/kitap_ekle.Designer.cs
@@ -48,123 +48,136 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 22);
+            label1.Location = new Point(14, 29);
             label1.Name = "label1";
-            label1.Size = new Size(58, 15);
+            label1.Size = new Size(73, 20);
             label1.TabIndex = 0;
             label1.Text = "kitap ismi";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(97, 14);
+            textBox1.Location = new Point(111, 19);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
+            textBox1.Size = new Size(114, 27);
             textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(97, 57);
+            textBox2.Location = new Point(111, 76);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(100, 23);
+            textBox2.Size = new Size(114, 27);
             textBox2.TabIndex = 3;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(12, 65);
+            label2.Location = new Point(14, 87);
             label2.Name = "label2";
-            label2.Size = new Size(58, 15);
+            label2.Size = new Size(72, 20);
             label2.TabIndex = 2;
             label2.Text = "kitap türü";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(12, 107);
+            label3.Location = new Point(14, 143);
             label3.Name = "label3";
-            label3.Size = new Size(53, 15);
+            label3.Size = new Size(69, 20);
             label3.TabIndex = 2;
             label3.Text = "yazar adı";
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(97, 99);
+            textBox3.Location = new Point(111, 132);
+            textBox3.Margin = new Padding(3, 4, 3, 4);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(100, 23);
+            textBox3.Size = new Size(114, 27);
             textBox3.TabIndex = 3;
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(12, 199);
+            label4.Location = new Point(14, 265);
             label4.Name = "label4";
-            label4.Size = new Size(65, 15);
+            label4.Size = new Size(82, 20);
             label4.TabIndex = 2;
             label4.Text = "baskı sayısı";
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(97, 191);
+            textBox4.Location = new Point(111, 255);
+            textBox4.Margin = new Padding(3, 4, 3, 4);
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(100, 23);
+            textBox4.Size = new Size(114, 27);
             textBox4.TabIndex = 3;
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(12, 153);
+            label5.Location = new Point(14, 204);
             label5.Name = "label5";
-            label5.Size = new Size(60, 15);
+            label5.Size = new Size(76, 20);
             label5.TabIndex = 2;
             label5.Text = "basım yılı ";
             // 
             // textBox5
             // 
-            textBox5.Location = new Point(97, 145);
+            textBox5.Location = new Point(111, 193);
+            textBox5.Margin = new Padding(3, 4, 3, 4);
             textBox5.Name = "textBox5";
-            textBox5.Size = new Size(100, 23);
+            textBox5.Size = new Size(114, 27);
             textBox5.TabIndex = 3;
             // 
             // kitapeklemebutonu
             // 
-            kitapeklemebutonu.Location = new Point(2, 254);
+            kitapeklemebutonu.Location = new Point(2, 339);
+            kitapeklemebutonu.Margin = new Padding(3, 4, 3, 4);
             kitapeklemebutonu.Name = "kitapeklemebutonu";
-            kitapeklemebutonu.Size = new Size(75, 23);
+            kitapeklemebutonu.Size = new Size(86, 31);
             kitapeklemebutonu.TabIndex = 4;
             kitapeklemebutonu.Text = "ekle";
             kitapeklemebutonu.UseVisualStyleBackColor = true;
+            kitapeklemebutonu.Click += kitapeklemebutonu_Click;
             // 
             // kitapsilmebutonu
             // 
-            kitapsilmebutonu.Location = new Point(83, 254);
+            kitapsilmebutonu.Location = new Point(95, 339);
+            kitapsilmebutonu.Margin = new Padding(3, 4, 3, 4);
             kitapsilmebutonu.Name = "kitapsilmebutonu";
-            kitapsilmebutonu.Size = new Size(75, 23);
+            kitapsilmebutonu.Size = new Size(86, 31);
             kitapsilmebutonu.TabIndex = 5;
             kitapsilmebutonu.Text = "silme";
             kitapsilmebutonu.UseVisualStyleBackColor = true;
+            kitapsilmebutonu.Click += kitapsilmebutonu_Click;
             // 
             // kitapguncellemebutonu
             // 
-            kitapguncellemebutonu.Location = new Point(164, 254);
+            kitapguncellemebutonu.Location = new Point(187, 339);
+            kitapguncellemebutonu.Margin = new Padding(3, 4, 3, 4);
             kitapguncellemebutonu.Name = "kitapguncellemebutonu";
-            kitapguncellemebutonu.Size = new Size(75, 23);
+            kitapguncellemebutonu.Size = new Size(86, 31);
             kitapguncellemebutonu.TabIndex = 6;
             kitapguncellemebutonu.Text = "güncelle";
             kitapguncellemebutonu.UseVisualStyleBackColor = true;
+            kitapguncellemebutonu.Click += kitapguncellemebutonu_Click;
             // 
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(352, 22);
+            dataGridView1.Location = new Point(402, 29);
+            dataGridView1.Margin = new Padding(3, 4, 3, 4);
             dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(415, 403);
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(474, 537);
             dataGridView1.TabIndex = 7;
             // 
             // kitap_ekle
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 600);
             Controls.Add(dataGridView1);
             Controls.Add(kitapguncellemebutonu);
             Controls.Add(kitapsilmebutonu);
@@ -179,6 +192,7 @@
             Controls.Add(label2);
             Controls.Add(textBox1);
             Controls.Add(label1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "kitap_ekle";
             Text = "kitap_ekle";
             Load += kitap_ekle_Load;
--- gorselprogramlamavize/kitap_ekle.cs
+++ gorselprogramlamavize/kitap_ekle.cs
@@ -1,25 +1,128 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SQLite;
 using System.Windows.Forms;
 
 namespace gorselprogramlamavize
 {
     public partial class kitap_ekle : Form
     {
+        private SQLiteConnection connection;
+        private SQLiteDataAdapter dataAdapter;
+        private DataTable dataTable;
+
         public kitap_ekle()
         {
             InitializeComponent();
+            InitializeDatabase();
+        }
+
+        private void InitializeDatabase()
+        {
+            connection = new SQLiteConnection("Data Source=database.db;Version=3;");
+            string createTableQuery = @"CREATE TABLE IF NOT EXISTS Kitaplar (
+                                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                        kitapismi TEXT,
+                                        kitapturu TEXT,
+                                        yazaradi TEXT,
+                                        basimyili TEXT,
+                                        baskisayisi TEXT)";
+
+            SQLiteCommand command = new SQLiteCommand(createTableQuery, connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        private void LoadData()
+        {
+            dataAdapter = new SQLiteDataAdapter("SELECT * FROM Kitaplar", connection);
+            dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;
         }
 
         private void kitap_ekle_Load(object sender, EventArgs e)
         {
+            LoadData();
+        }
 
+        private void kitapeklemebutonu_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            string insertQuery = @"INSERT INTO Kitaplar (kitapismi, kitapturu, yazaradi, basimyili, baskisayisi) 
+                                    VALUES (@kitapismi, @kitapturu, @yazaradi, @basimyili, @baskisayisi)";
+
+            SQLiteCommand command = new SQLiteCommand(insertQuery, connection);
+            command.Parameters.AddWithValue("@kitapismi", textBox1.Text);
+            command.Parameters.AddWithValue("@kitapturu", textBox2.Text);
+            command.Parameters.AddWithValue("@yazaradi", textBox3.Text);
+            command.Parameters.AddWithValue("@basimyili", textBox5.Text);
+            command.Parameters.AddWithValue("@baskisayisi", textBox4.Text);
+
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            MessageBox.Show("Veri başarıyla eklendi.");
+
+            // Verileri yeniden yükle
+            LoadData();
+        }
+
+        private void kitapsilmebutonu_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                connection.Open();
+                string deleteQuery = "DELETE FROM Kitaplar WHERE id=@id";
+
+                SQLiteCommand command = new SQLiteCommand(deleteQuery, connection);
+                command.Parameters.AddWithValue("@id", dataGridView1.SelectedRows[0].Cells["id"].Value);
+
+                command.ExecuteNonQuery();
+                connection.Close();
+
+                MessageBox.Show("Veri başarıyla silindi.");
+
+                // Verileri yeniden yükle
+                LoadData();
+            }
+            else
+            {
+                MessageBox.Show("Lütfen bir satır seçin.");
+            }
+        }
+
+        private void kitapguncellemebutonu_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                connection.Open();
+                string updateQuery = @"UPDATE Kitaplar 
+                                        SET kitapismi=@kitapismi, kitapturu=@kitapturu, yazaradi=@yazaradi, 
+                                            basimyili=@basimyili, baskisayisi=@baskisayisi 
+                                        WHERE id=@id";
+
+                SQLiteCommand command = new SQLiteCommand(updateQuery, connection);
+                command.Parameters.AddWithValue("@kitapismi", textBox1.Text);
+                command.Parameters.AddWithValue("@kitapturu", textBox2.Text);
+                command.Parameters.AddWithValue("@yazaradi", textBox3.Text);
+                command.Parameters.AddWithValue("@basimyili", textBox5.Text);
+                command.Parameters.AddWithValue("@baskisayisi", textBox4.Text);
+                command.Parameters.AddWithValue("@id", dataGridView1.SelectedRows[0].Cells["id"].Value);
+
+                command.ExecuteNonQuery();
+                connection.Close();
+
+                MessageBox.Show("Veri başarıyla güncellendi.");
+
+                // Verileri yeniden yükle
+                LoadData();
+            }
+            else
+            {
+                MessageBox.Show("Lütfen bir satır seçin.");
+            }
         }
     }
 }
